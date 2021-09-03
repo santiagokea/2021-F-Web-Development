e10933e5f09d0fe7b24316fdd529baf3a3b3dd65
Santiago Donoso <sand@kea.dk>
2021-09-03 09:50:12 +0200

    ...

---
+++ app.js
@@ -0,0 +1,32 @@
+var aLetters = ["a", "b", "c", "d", "e"]
+for(var i = 0; i < aLetters.length; i++){
+  if( i == 1 && aLetters[i] == "b" ) continue 
+  console.log(aLetters[i])
+}
+// if {}
+// else()
+// Show all letters except the letter in
+// index 1 if it is a b
+
+
+
+
+
+
+// Create a loop that shows all letters
+// except the letter which is in index 1
+
+
+
+
+// aLetters.forEach( sLetter => {
+//   console.log(sLetter)
+// } )
+
+
+
+// aLetters.foreach( function(sLetter){
+
+// } )
+
+
