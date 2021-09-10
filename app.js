9fed35750f4057ae32cfbff3ac8b48537608021a
Santiago Donoso <sand@kea.dk>
2021-09-10 09:26:40 +0200

    ...

--- app.js
+++ app.js
@@ -1,12 +1,17 @@
 function one(q){ return document.querySelector(q) }
 function all(q){ return document.querySelectorAll(q) }
 
+
 // Compoment
 let item = {
 
   saveItem : function(){
     const itemName = one("#itemName").value
-    let divItem = `<div class="item">${itemName}</div>`
+    let divItem = ` <div class="item">
+                      <div>${itemName}</div>
+                      <div>🗑️</div>
+                    </div>`
+
     one("#items").insertAdjacentHTML('afterbegin', divItem) 
     one("#itemName").value = ""
   } 
