de009ccd72fd6194f237fb8eea44451e3b121b1f
Santiago Donoso <sand@kea.dk>
2021-09-10 09:15:59 +0200

    ...

--- app.js
+++ app.js
@@ -2,7 +2,9 @@
 let item = {
 
   saveItem : function(){
-    console.log("saving item...")
+    const itemName = document.querySelector("#itemName").value
+    let divItem = `<div class="item">${itemName}</div>`
+    document.querySelector("#items").insertAdjacentHTML('afterbegin', divItem) 
   } 
 
 }
