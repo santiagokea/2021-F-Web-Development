7f822d1e19d55d18d2a00f2def69010e717a7038
Santiago Donoso <sand@kea.dk>
2021-09-17 10:13:38 +0200

    ...

--- associative-arrays.php
+++ associative-arrays.php
@@ -1,15 +1,33 @@
 <?php
 // multidimensional associative array
-$people = [
-  [],
-  [],
-  []
+$users = [
+  ['name'=>'A', 'email'=>'@a', 'password'=>'passA'],
+  ['name'=>'B', 'email'=>'@b', 'password'=>'passB']
 ];
-
 // Associative array
 // $person = [ 'name' => 'Santiago', 'last_name' => 'Donoso' ];
 // echo $person['name'].' '.$person['last_name'];
 // echo "{$person['name']} {$person['last_name']}";
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+<body>
+  <div id="users">
+    <?php
+    foreach($users as $user){
+      echo "<div>{$user['name']}</div>";
+    }
+    ?>
+  </div>
+</body>
+</html>
 
 
 
