e86330c8b59d334a0633597c0d98c99172ec57e1
Santiago Donoso <sand@kea.dk>
2021-09-24 09:02:57 +0200

    ...

--- api-signup.php
+++ api-signup.php
@@ -1,15 +1,22 @@
 <?php
-// APIs almost never reply with HTML
-// APIs almost always reply with JSON and HTTP status codes
-// 20x means ok. We will use 200
-// 30x means redirect - In APIs hardly used
-// 40x means client error - We will use 400
-// 50x means server error - We will use 500
-// POST data
-// http_response_code(200);
-// the dot . means concatenation
-// echo $_POST['name'].' '.$_POST['last_name'];
-echo "{$_POST['name']} {$_POST['last_name']}";
+// VALIDATE THE DATA
+if( ! isset( $_POST['name'] ) ){
+  http_response_code(400);
+  echo "name is required";
+  exit(); // die() // exit;
+}
+
+if( strlen( $_POST['name'] ) < 2 ){
+  http_response_code(400);
+  echo "name must be at least 2 characters";
+  exit(); // die() // exit;
+}
+
+
+
+// SUCCESS
+echo "You are signed up";
+
 
 
 
