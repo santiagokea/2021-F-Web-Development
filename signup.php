c96b9e431ea91b88497005a15410b28ede9f26c6
Santiago Donoso <sand@kea.dk>
2021-09-24 11:38:46 +0200

    ...

---
+++ signup.php
@@ -0,0 +1,16 @@
+<?php
+$_title = 'Signup';
+require_once('components/header.php');
+?>
+
+<h1>SIGNUP</h1>
+<form>
+  <input type="text" placeholder="name">
+  <input type="text" placeholder="last name">
+  <input type="text" placeholder="email">
+  <button>Signup</button>
+</form>
+
+<?php
+require_once('components/footer.php');
+?>
