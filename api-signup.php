4a4c6642396776b1ffaec7acf5a08b8ead48f6da
Santiago Donoso <sand@kea.dk>
2021-09-24 09:10:39 +0200

    ...

--- api-signup.php
+++ api-signup.php
@@ -1,21 +1,21 @@
 <?php
+
 // VALIDATE THE DATA
-if( ! isset( $_POST['name'] ) ){
-  http_response_code(400);
-  echo "name is required";
-  exit(); // die() // exit;
-}
-
-if( strlen( $_POST['name'] ) < 2 ){
-  http_response_code(400);
-  echo "name must be at least 2 characters";
-  exit(); // die() // exit;
-}
-
+if( ! isset( $_POST['name'] ) ){ send_error(); }
+if( strlen( $_POST['name'] ) < 2 ){ send_error(); }
+if( strlen( $_POST['name'] ) > 5 ){ send_error(); }
 
 
 // SUCCESS
 echo "You are signed up";
+
+
+// function to manage responding in case of an error
+function send_error(){
+  http_response_code(400);
+  echo "xxxxxxxxxxx";
+  exit();
+}
 
 
 
