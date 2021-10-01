46399f2d7a0dddcd1f8dff8737c52c01f204614a
Santiago Donoso <sand@kea.dk>
2021-10-01 09:58:40 +0200

    ...

--- api-login.php
+++ api-login.php
@@ -13,14 +13,12 @@ if( ! isset($_POST['password']) ){ _res(400,['info'=>'password required']); }
 if(strlen($_POST['password']) < _PASSWORD_MIN_LEN){ _res(400,['info'=>'password min '._PASSWORD_MIN_LEN.' characters']); }
 if(strlen($_POST['password']) > _PASSWORD_MAX_LEN){ _res(400,['info'=>'password max '._PASSWORD_MAX_LEN.' characters']); }
 
-
 // Expected fake variables
 $correct_email = 'a@a.com';
 $correct_password = 'password';
 
 if( $correct_email != $_POST['email'] || $correct_password != $_POST['password'] ){
   _res(400, ['info'=>'wrong credentials']);
-
 }
 
 // Success
