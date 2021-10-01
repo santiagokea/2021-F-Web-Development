897dd71be7ad1fecd8eacddc2b9accdc321b4bc0
Santiago Donoso <sand@kea.dk>
2021-10-01 11:27:08 +0200

    ...

--- api-login.php
+++ api-login.php
@@ -17,12 +17,6 @@ if(strlen($_POST['password']) > _PASSWORD_MAX_LEN){ _res(400,['info'=>'password 
 $correct_email = 'a@a.com';
 $correct_password = 'password';
 
-// Connect to the db, conside a try catch
-// Run a SQL command to check if email and password match
-// If there is a match, put the row in the session, this means
-// that the session will have all the data for the user
-// $_SESSION['user'] = $row
-
 if( $correct_email != $_POST['email'] || $correct_password != $_POST['password'] ){
   _res(400, ['info'=>'wrong credentials']);
 }
