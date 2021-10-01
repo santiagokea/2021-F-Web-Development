b75adf839c4c9e68817cc6c1d0f6e519567106cb
Santiago Donoso <sand@kea.dk>
2021-10-01 10:24:04 +0200

    ...

---
+++ login.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+<body>
+
+  <form onsubmit="return false">
+    <input name="email" type="text" placeholder="email">
+    <input name="password" type="password" placeholder="password">
+    <button onclick="login()">Login</button>
+  </form>
+
+  <script>
+    async function login(){
+      const form = event.target.form
+      console.log(form)
+      let conn = await fetch("api-login", {
+        method: "POST",
+        body: new FormData(form)
+      })
+      let res = await conn.json()
+      console.log(res)
+    }
+  </script>
+  
+</body>
+</html>
