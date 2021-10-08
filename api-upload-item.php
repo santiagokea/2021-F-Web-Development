fc57ae2a11c2d4ab902026e5f7bc6694c7b77b7e
Santiago Donoso <sand@kea.dk>
2021-10-08 10:20:46 +0200

    ...

--- api-upload-item.php
+++ api-upload-item.php
@@ -4,11 +4,9 @@ require_once('globals.php');
 
 // Validate
 if(!isset($_POST['item_name'])){ http_response_code(400); echo 'item_name required'; exit(); }
-if(strlen($_POST['item_name']) < _ITEM_MIN_LEN){ 
-  http_response_code(400); 
-  echo 'item_name min '._ITEM_MIN_LEN.' characters'; 
-  exit(); 
-}
+if(strlen($_POST['item_name']) < _ITEM_MIN_LEN){ http_response_code(400); echo 'item_name min '._ITEM_MIN_LEN.' characters'; exit(); }
+if(strlen($_POST['item_name']) > _ITEM_MAX_LEN){ http_response_code(400); echo 'item_name max '._ITEM_MAX_LEN.' characters'; exit(); }
+
 
 
 
