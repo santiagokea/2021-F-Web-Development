11e10dabe24812106f3e682611d9dc8a55e3b40b
Santiago Donoso <sand@kea.dk>
2021-10-08 09:28:58 +0200

    ...

---
+++ dictionary.php
@@ -0,0 +1,21 @@
+<?php
+
+$text = [
+  '1' => [
+    'en' => 'Hi',
+    'dk' => 'Hej',
+    'es' => 'Hola'
+  ],
+  '2' => [
+    'en' => 'Welcome',
+    'dk' => 'Velkommen',
+    'es' => 'Bienvenido'
+  ]
+];
+
+
+
+
+
+
+
